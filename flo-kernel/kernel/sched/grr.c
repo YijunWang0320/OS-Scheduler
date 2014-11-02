d6e71f319bb6028fc05cb42d5e3b5b700892de3d
WangYijun <yw2676@columbia.edu>
2014-11-02 15:31:01 -0500

    add timer

--- flo-kernel/kernel/sched/grr.c
+++ flo-kernel/kernel/sched/grr.c
@@ -6,6 +6,21 @@
 
 #include <linux/slab.h>
 
+static enum hrtimer_restart do_load_balance_grr(struct hrtimer *timer) {
+	ktime_t load_balance_timer;
+	struct timespec timer_spec = {
+		.tv_nsec = 50000000;
+		.tv_sec = 0;
+	};
+	load_balance_timer = timespec_to_ktime(timer_spec);
+	printk("every 5ms\n");
+#ifdef CONFIG_SMP
+	load_balance_grr();
+#endif
+	ktime_t now = timer->base->get_time();
+	hrtimer_forward(timer,now,load_balance_timer);
+	return HRTIMER_RESTART;
+}
 static void enqueue_task_grr(struct rq *rq, struct task_struct *p, int wakeup)
 {
 	p->grr.time_slice = RR_TIMESLICE;
