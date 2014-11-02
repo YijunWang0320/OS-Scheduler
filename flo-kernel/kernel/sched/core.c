663d2aa7c701eee0947c0f27bdd81479c9d88282
Wei Dai <wdai2248@columbia.edu>
2014-11-02 17:17:35 -0500

    test SMP2

--- flo-kernel/kernel/sched/core.c
+++ flo-kernel/kernel/sched/core.c
@@ -88,7 +88,7 @@
 #include <trace/events/sched.h>
 
 ATOMIC_NOTIFIER_HEAD(migration_notifier_head);
-DECLARE_PER_CPU(struct hrtimer,load_balance_ktime);
+static struct hrtimer load_balance_ktime;
 
 void start_bandwidth_timer(struct hrtimer *period_timer, ktime_t period)
 {
@@ -6907,12 +6907,8 @@ void __init sched_init_smp(void)
 	init_hrtick();
 
 	/*add our timer start*/
-<<<<<<< HEAD
-=======
 	hrtimer_init(&load_balance_ktime,CLOCK_MONOTONIC,HRTIMER_MODE_REL);
-	load_balance_ktime.function = do_load_balance_grr;
->>>>>>> d857f09cf938676ae104ff4531e5774eb295d6b4
-	hrtimer_start(&load_balance_ktime,load_balance_timer,HRTIMER_MODE_REL);
+
 
 	/* Move init over to a non-isolated CPU */
 	if (set_cpus_allowed_ptr(current, non_isolated_cpus) < 0)
@@ -6987,6 +6983,8 @@ void __init sched_init(void)
 
 #ifdef CONFIG_SMP
 	init_defrootdomain();
+	load_balance_ktime.function = do_load_balance_grr;
+	hrtimer_start(&load_balance_ktime,load_balance_timer,HRTIMER_MODE_REL);
 #endif
 
 	init_rt_bandwidth(&def_rt_bandwidth,
